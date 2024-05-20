49f99568d3e297def0966d9221353b1335094f65
Yuxin Zhang <Yuxinzhang2024@u.northwestern.edu>
2024-05-20 21:33:52 +0000

    1

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,20 @@
+class EntriesController < ApplicationController
+  def index
+    @entriess = Entry.all
+  end
+
+  def new
+    @entry = Entry.new
+    @entry["place_id"] = params["place_id"]
+  end
+
+  def create
+    @entry = Entry.new
+    @entry["title"] = params["entry"]["title"]
+    @entry["description"] = params["entry"]["description"]
+    @entry["posted_on"] = params["entry"]["posted_on"]
+    @entry["place_id"] = params["entry"]["place_id"]
+    @entry.save
+    redirect_to "/places/#{@entry["place_id"]}"
+  end  
+end
